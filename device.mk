aa7505f88a4459b07d70d369000dfe6938843f75
Paul Keith <javelinanddart@gmail.com>
2019-06-09 16:39:09 +0200

    hammerhead: Build livedisplay sysfs service
     * Add supported interfaces to manifest while we're at it
    
    Change-Id: Iaad13fcdfb603d144599133339f9c0c6a0d1c66a

--- device.mk
+++ device.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
