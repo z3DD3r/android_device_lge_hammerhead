cfafd89a6eff74298b79842a012301dc1c96a075
Sujin Panicker <spanic@codeaurora.org>
2019-07-18 14:39:28 +0530

    hammerhead: media: Fix rank of OMX audio codec components
    Rank OMX SW audio codecs higher than C2 codecs.
    
    Change-Id: I7d03179ebd313d2592512178eeef266b4243f2f0

--- device.mk
+++ device.mk
@@ -366,6 +366,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
     wifi.supplicant_scan_interval=15
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0
+
 # Enable AAC 5.1 output
 PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true
