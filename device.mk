e1d88550001eb7b926e16ed6351420af0479b6d1
z3DD3r <2339201+z3DD3r@users.noreply.github.com>
2020-04-09 17:35:49 +0300

    hammerhead: Use binderized allocator HAL

--- device.mk
+++ device.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     gralloc.msm8974 \
     libgenlock \
