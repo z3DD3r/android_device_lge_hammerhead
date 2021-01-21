f0c658ed36a0b1b39ce4ff6f9e1a1f7186ba9cf9
Kevin F. Haggerty <haggertk@lineageos.org>
2021-01-21 10:02:12 -0700

    hammerhead: Drop OVERRIDE_RS_DRIVER
    * So, this one from bullhead can cause everything to get janky AF
      every time RenderScript is actually used, and finding one that
      actually loads (no missing symbols) AND actually works well is,
      well, challenging. The device works fine with the "normal"
      non-adreno driver in AOSP, so let's use that until/unless we find
      an actually working adreno driver.
    
    Change-Id: I4ab19269eb49f5f2aed375d9055d68c1b9835f69

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,7 +124,6 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 DEVICE_MANIFEST_FILE := device/lge/hammerhead/manifest.xml
 DEVICE_MATRIX_FILE := device/lge/hammerhead/compatibility_matrix.xml
 
-OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 TARGET_HAS_HH_VSYNC_ISSUE := true
 
